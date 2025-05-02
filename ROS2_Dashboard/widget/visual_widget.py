ac5e7582349c7890951b000a20d8d578a814c1e4
haidarpriatama <haidar.rafi.ap@gmail.com>
2025-05-02 19:50:20 +0700

    Add new_map.png and update visual_widget

--- ROS2_Dashboard/widget/visual_widget.py
+++ ROS2_Dashboard/widget/visual_widget.py
@@ -65,7 +65,7 @@ def create_visual_widget(dashboard):
     
     # Load field image
     try:
-        img = plt.imread("/home/haidar/ros2_git/src/ROS2-Dashboard/ROS2_Dashboard/resources/map.png")
+        img = plt.imread("/home/haidar/ros2_git/src/ROS2-Dashboard/ROS2_Dashboard/resources/new_map.png")
         dashboard.field_img = dashboard.ax.imshow(img, extent=[0, 1500, 0, 800])  # Set map scale to 1500 x 800
         
         # Set up plot
